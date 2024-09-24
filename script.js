a2743d47200c810399888fef59b38bf613f4323b
emmamcmillin <48865335+emmamcmillin@users.noreply.github.com>
2024-09-24 13:09:21 -0600

    added all the files

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function toggleNav() {
+    const nav = document.getElementById('navbar');
+    nav.classList.toggle('active'); // Toggle the "active" class on nav
+}
+
+function toggleDarkMode() {
+    document.body.classList.toggle('dark-mode');
+}
+
+
